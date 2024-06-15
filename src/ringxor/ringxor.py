30aa6cc0cc7e7a43e1f54134771cadae928266a3
Mitchell Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2024-06-15 16:08:29 -0400

    chore: track with dicts instead of tuples

--- src/ringxor/ringxor.py
+++ src/ringxor/ringxor.py
@@ -7,20 +7,20 @@ key_image_pointer: Type = Union[int, str, Any]
 output_pointer: Type = Union[int, str, Any]
 ring_vector: Type = Set[output_pointer]
 ring_bucket: Type = Dict[key_image_pointer, ring_vector]
-edge: Type = Tuple[key_image_pointer, output_pointer]
+edge: Type = Dict[str, Any]
 
 
 def process_bucket_single_thread_core(
     rings: ring_bucket,
     index_pairs: Union[List[Tuple[int, int]], None],
-) -> List[Tuple[key_image_pointer, output_pointer]]:
+) -> List[edge]:
     """
     Core function - you do not need to interact with this directly, use `process_bucket()` below
     This takes a batch of index pairs and processes the rings in a single thread.
 
     :param rings: bucket of rings to analyze
     :param index_pairs: list of index pairs to process. If None provided, checks all possible combinations
-    :return: identified transaction tree edges in the form of (key_image, output)
+    :return: identified transaction tree edges in the form of {"key_image_pointer": key_image, "output_pointer": output}
     """
     # If no index pairs are provided (could be None, (), {}, [], etc...), use all possible combinations
     if not index_pairs:
@@ -32,14 +32,14 @@ def process_bucket_single_thread_core(
     }
 
     # Process the bucket
-    edges: List[Tuple[key_image_pointer, output_pointer]] = []
+    edges: List[edge] = []
     for key_image_pointer_left, key_image_pointer_right in key_image_pointer_pairs:
         ring_left: ring_vector = rings[key_image_pointer_left]
         ring_right: ring_vector = rings[key_image_pointer_right]
         # Do we have a singleton?
         if len(ring_left & ring_right) == len(ring_left) - 1:
-            edges.append((key_image_pointer_left, (ring_left - ring_right).pop()))
-            edges.append((key_image_pointer_right, (ring_right - ring_left).pop()))
+            edges.append({"key_image_pointer": key_image_pointer_left, "output_pointer": (ring_left - ring_right).pop()})
+            edges.append({"key_image_pointer": key_image_pointer_right, "output_pointer": (ring_right - ring_left).pop()})
 
     return edges
 
@@ -48,7 +48,7 @@ def process_bucket(
     rings: ring_bucket,
     index_pairs: Union[None, Collection[Tuple[int, int]]] = None,
     num_workers: Union[int, None] = None,
-) -> List[Tuple[key_image_pointer, output_pointer]]:
+) -> List[edge]:
     """
     This function takes a bucket of rings and processes them to identify transaction tree edges from singletons
     that arise from XORing ring pairs (i.e. symmetric set difference)
@@ -56,7 +56,7 @@ def process_bucket(
     :param rings: bucket of rings to analyze
     :param index_pairs: list of index pairs to process. If None provided, checks all possible combinations
     :param num_workers: number of workers to use. If None provided, uses all available cores
-    :return: identified transaction tree edges in the form of (key_image, output)
+    :return: identified transaction tree edges in the form of {"key_image_pointer": key_image, "output_pointer": output}
     """
     # If no index pairs are provided, use all possible combinations
     if index_pairs is None:
@@ -81,12 +81,12 @@ def process_bucket(
 
         # Process the chunks in parallel
         with Pool(processes=num_workers) as pool:
-            results: List[List[Tuple[key_image_pointer, output_pointer]]] = pool.starmap(
+            results: List[List[edge]] = pool.starmap(
                 process_bucket_single_thread_core,
                 iterable,
             )
         # Combine the results
-        edges: List[Tuple[key_image_pointer, output_pointer]] = []
+        edges: List[edge] = []
         for result in results:
             edges.extend(result)
         return edges
--- tests/test_ringxor.py
+++ tests/test_ringxor.py
@@ -13,45 +13,45 @@ all_rings: ringxor.ring_bucket = {key_image: set(ring) for key_image, ring in al
 
 
 def test_ringxor_process_bucket_single_thread_core():
-    results = sorted(ringxor.process_bucket_single_thread_core(all_rings, index_pairs=None))
+    results = sorted(ringxor.process_bucket_single_thread_core(all_rings, index_pairs=None), key=lambda x: x['key_image_pointer'])
 
     assert len(results) == 54
-    assert results[0] == (
-        "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
-        "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
-    )
-    assert results[53] == (
-        "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
-        "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
-    )
+    assert results[0] == {
+        "key_image_pointer": "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
+        "output_pointer": "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
+    }
+    assert results[53] == {
+        "key_image_pointer": "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
+        "output_pointer": "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
+    }
 
 
 def test_ringxor_process_bucket_1_worker():
-    results = sorted(ringxor.process_bucket(all_rings, index_pairs=None, num_workers=1))
+    results = sorted(ringxor.process_bucket(all_rings, index_pairs=None, num_workers=1), key=lambda x: x['key_image_pointer'])
 
     assert len(results) == 54
-    assert results[0] == (
-        "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
-        "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
-    )
-    assert results[53] == (
-        "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
-        "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
-    )
+    assert results[0] == {
+        "key_image_pointer": "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
+        "output_pointer": "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
+    }
+    assert results[53] == {
+        "key_image_pointer": "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
+        "output_pointer": "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
+    }
 
 
 def test_ringxor_process_bucket_N_worker():
     if cpu_count() == 1:
         print("Skipping test_ringxor_process_bucket_N_worker because only 1 CPU is available")
     else:
-        results = sorted(ringxor.process_bucket(all_rings, index_pairs=None, num_workers=2))
+        results = sorted(ringxor.process_bucket(all_rings, index_pairs=None, num_workers=2), key=lambda x: x['key_image_pointer'])
 
         assert len(results) == 54
-        assert results[0] == (
-            "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
-            "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
-        )
-        assert results[53] == (
-            "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
-            "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
-        )
+        assert results[0] == {
+            "key_image_pointer": "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
+            "output_pointer": "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
+        }
+        assert results[53] == {
+            "key_image_pointer": "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
+            "output_pointer": "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
+        }
